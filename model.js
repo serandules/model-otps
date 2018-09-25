6cfee226f31a1ab6b3c11babc1c8f941bd2d5741
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-09-25 19:51:25 +0530

    updating

--- model.js
+++ model.js
@@ -5,11 +5,20 @@ var Schema = mongoose.Schema;
 var mongins = require('mongins');
 var types = require('validators').types;
 
-var otp = Schema({}, {collection: 'otps'});
+var otp = Schema({
+    value: {
+        type: String,
+        required: true,
+        server: true,
+        validator: types.string({
+            length: 96
+        })
+    }
+}, {collection: 'otps'});
 
 otp.plugin(mongins);
 otp.plugin(mongins.user);
-otp.plugin(mongins.createdAt);
-otp.plugin(mongins.updatedAt);
+otp.plugin(mongins.createdAt());
+otp.plugin(mongins.updatedAt({expires: 600}));
 
 module.exports = mongoose.model('otps', otp);
